0e664481e3d5239bea7339e6e9993f4c7a375108
Siddiqui Asif Ahmed <157271879+asifsiddiqui9011@users.noreply.github.com>
2024-06-15 17:51:55 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Siddiqui Asif Ahmed",
+  title: "Full Stack Developer",
+  email: "asifsiddiqui9011@gmail.com",
+  gitHub: "asifsiddiqui9011",
+  instagram: "as___if___sidd",
+  linkedIn: "siddiquiasifahmed9011",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
